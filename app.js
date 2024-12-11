3987546b2f255299a898f8fd947ab763ce3e93c1
knapkejp <70207913+knapkejp@users.noreply.github.com>
2024-12-10 22:02:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-const express = require('express');  // Import express
-const app = express();  // Initialize the app
-const port = process.env.PORT || 3000;  // Default to 3000 for local, Heroku provides its own port
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
 // Global variables
 let allVideos = [];  // Store all videos
 let filteredVideos = [];  // Store the filtered videos
